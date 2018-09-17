a7347ebad7171b4a294f0235eff1e4d1f4ec5980
FrancisFan98 <1125180016@qq.com>
2018-09-17 15:22:06 -0500

    second a1

--- csci 2041 ocaml/a1-code/abovefuncs.ml
+++ csci 2041 ocaml/a1-code/abovefuncs.ml
@@ -1,10 +1,26 @@
 let array_above thresh arr = 
-  let rs = ref [||] in 
+  let len = ref 0 in 
+  for i = 0 to Array.length arr - 1 do
+    if arr.(i) > thresh then
+      len := !len + 1
+  done;
+  let rs = ref (Array.make !len thresh) and index = ref 0 in
+  for i = 0 to Array.length arr - 1 do
+    if arr.(i) > thresh then begin
+      !rs.(!index) <- arr.(i);
+      index := !index + 1
+    end
+  done;
+  !rs
+;; 
+
+
+  (*let rs = ref [||] in 
   for i = 0 to Array.length arr - 1 do 
     if arr.(i) > thresh then
       rs := (Array.append !rs [|arr.(i)|])
      done;
-   !rs
+   !rs*)
 ;;
 
 
